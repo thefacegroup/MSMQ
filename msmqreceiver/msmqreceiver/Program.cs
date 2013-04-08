3f00dd806573d7ab040ac9948db7d7db8832d7d2
Brian <brianccain@gmail.com>
2013-04-08 12:18:51 -0500

    XMLError, but should be working

--- msmqreceiver/msmqreceiver/Program.cs
+++ msmqreceiver/msmqreceiver/Program.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Messaging;
+using System.Runtime.Serialization;
 
 namespace msmqreceiver
 {
     class Program
     {
+        private const string MESSAGE_QUEUE = @".\Private$\SampleQueue";
+
+        private static void CheckMessage()
+        {
+            try
+            {
+                var queue = new MessageQueue(MESSAGE_QUEUE);
+                var message = queue.Receive(new TimeSpan(0, 0, 1));
+
+                message.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+                Console.WriteLine(message.Body.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No message");
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Recieve me");
             while (true)
             {
-
+                CheckMessage();
             }
         }
     }
--- msmqsender/msmqsender/Program.cs
+++ msmqsender/msmqsender/Program.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Messaging;
+using System.Runtime.Serialization;
+
+/*
+ * Based off of the tutorial found here: http://fransiscuss.com/2012/06/01/msmq-basic-tutorial/
+ * @bccain
+ */
 
 namespace msmqsender
 {
     class Program
     {
+        private const string MESSAGE_QUEUE = @".\Private$\SampleQueue";
+
+        private MessageQueue _queue;
+
+        private void SendMessage(string message)
+        {
+            _queue = new MessageQueue(MESSAGE_QUEUE);
+            Message msg = new Message();
+            msg.Label = "Presentation at " + DateTime.Now.ToString();
+            _queue.Send(msg);
+
+            //lblError.Text = "Message already sent";
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Send me");
 
-            while (true)
-            {
-
-            }
+            Program p = new Program();
+            p.SendMessage("Hello thar");
         }
     }
 }
