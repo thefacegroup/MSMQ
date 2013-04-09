460fab57142edf38e66e439a6d2ee07266b961c9
Carlos <csalazar@win8vm>
2013-04-09 00:25:13 -0500

    Added XML serialization/deserialization code

--- msmqreceiver/msmqreceiver/Program.cs
+++ msmqreceiver/msmqreceiver/Program.cs
@@ -4,18 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Messaging;
-using System.Runtime.Serialization;
+//using System.Runtime.Serialization;
+using System.Xml.Serialization; //needed for XmlSerializer
+using System.IO; //needed for TextWriter
 
 /*
  * Based off of the tutorial found here: http://fransiscuss.com/2012/06/01/msmq-basic-tutorial/
  * @bccain
+ * 
+ * Xml Serialization : http://msdn.microsoft.com/en-us/library/58a18dwa.aspx 
+ * @csalazar
  */
 
 namespace msmqreceiver
 {
     class Program
     {
-        private const string MESSAGE_QUEUE = @".\Private$\receiveQueue";
+        private static List<String> thisWasSerialized = null;
+        private static XmlSerializer xs = new XmlSerializer(typeof(List<String>));
+
+
+        private const string MESSAGE_QUEUE = @".\Private$\servicequeue";
         //private const string REMOTE_QUEUE = "FormatName:Direct=TCP:10.27.41.4\\private$\\receiveQueue";
 
         private static void CheckMessage()
@@ -26,7 +35,18 @@ namespace msmqreceiver
                 var message = queue.Receive(new TimeSpan(0, 0, 1));
 
                 message.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
-                Console.WriteLine(message.Body.ToString());
+
+                // Lets just assume for this baby example that we know the message will be xml.
+
+                TextReader reader = new StringReader(message.Body.ToString());
+                thisWasSerialized = (List<String>)xs.Deserialize(reader);
+
+                foreach (String s in thisWasSerialized)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //Console.WriteLine(message.Body.ToString());
             }
             catch (Exception e)
             {
--- msmqsender/msmqsender/Program.cs
+++ msmqsender/msmqsender/Program.cs
@@ -4,18 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Messaging;
-using System.Runtime.Serialization;
+//using System.Runtime.Serialization; //unnecessary for this example at least
+using System.Xml.Serialization; //needed for XmlSerializer
+using System.IO; //needed for TextWriter
 
 /*
  * Based off of the tutorial found here: http://fransiscuss.com/2012/06/01/msmq-basic-tutorial/
  * @bccain
+ * 
+ * Xml Serialization : http://msdn.microsoft.com/en-us/library/58a18dwa.aspx 
+ * @csalazar
  */
 
 namespace msmqsender
 {
     class Program
     {
-        private const string MESSAGE_QUEUE = @".\Private$\sendqueue";
+        private List<String> serializeThis = null;
+        private XmlSerializer xs = null;
+        private TextWriter writer = null;
+
+        public Program()
+        {
+            serializeThis = new List<String>();
+            serializeThis.Add("one");
+            serializeThis.Add("two");
+            serializeThis.Add("three");
+
+            xs = new XmlSerializer(typeof(List<String>));
+            writer = new StringWriter(); //Instead of StreamWriter, we want to write to a string!
+        }
+
+
+        //private const string MESSAGE_QUEUE = @".\Private$\visualizerqueue";
+        private const string MESSAGE_QUEUE = @".\Private$\servicequeue";
+
         //private const string REMOTE_QUEUE = "FormatName:Direct=TCP:10.27.41.4\\private$\\receiveQueue";
 
         private MessageQueue _queue;
@@ -42,8 +65,16 @@ namespace msmqsender
             //    p.SendMessage("Heeellloooo wooorrrlllddddd message number #" + n++);
             //}
             String msg = "";
-            msg = Console.ReadLine();
+            //msg = Console.ReadLine();
+
+            p.xs.Serialize(p.writer, p.serializeThis);
+            msg = p.writer.ToString();
+            //Console.WriteLine(msg);
             p.SendMessage(msg);
+            while (true)
+            {
+                //derp
+            }
             
         }
     }
